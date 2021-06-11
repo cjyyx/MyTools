0aa5f79e60e3b565feff99939f10bafc4be2a462
cjy <1627814505@qq.com>
2021-06-11 22:41:01 +0800

    paddle相关工具

--- paddle相关工具/tempCodeRunnerFile.py
+++ paddle相关工具/tempCodeRunnerFile.py
@@ -1,6 +1,1 @@
-net = paddle.nn.Sequential(
-    paddle.nn.Linear(1, 8),
-    paddle.nn.ReLU(),
-    paddle.nn.Linear(8, 1),
-    paddle.nn.Tanh()
-)
+[a/li[-1] for a in li]
--- paddle相关工具/test.py
+++ paddle相关工具/test.py
@@ -1,7 +1,7 @@
 import paddle
-from control_parameters import *
+# from control_parameters import *
 from GD_fit import *
-
+from analyser import Analyser
 
 
 
@@ -16,26 +16,16 @@ def eval(net):
 net = paddle.nn.Sequential(
     paddle.nn.Linear(1, 8),
     paddle.nn.ReLU(),
-    paddle.nn.Linear(8, 1),
+    paddle.nn.Linear(8, 4),
+    paddle.nn.ReLU(),
+    paddle.nn.Linear(4, 1),
     paddle.nn.Tanh()
 )
 
-LR=0.1
-dx=0.5
+LR=1
+dx=1
 
-for b in range(100):
-    for i in range(get_parameter_size(net)):
-        loss=eval(net)
-        para=get_parameter(net,i)
-        set_parameter(net,i,para+dx)
-        d_loss=eval(net)-loss
-        if d_loss==0:
-            continue
-        grad=d_loss/dx
-        new_pare=para-LR*(loss/grad)
-        set_parameter(net,i,new_pare)
+GD_fit(net,eval,1)
 
-    if b%10==0:
-        print(loss.numpy()[0])
 
 
